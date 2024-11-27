6720b1fa97caf79fd13d2fc17c690f506268f3d6
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-26 23:02:06 -0600

    Command Pattern - 1

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Command Pattern
+ *
+ * With the Command Pattern, we can decouple objects that execute a certain task
+ * from the object that calls the method
+ */
+
+class OrderManager {
+    constructor() {
+        this.orders = []
+    }
+
+    placeOrder(order, id) {
+        this.orders.push(id)
+        return `You have successfully ordered ${order} (${id})`;
+    }
+
+    trackOrder(id) {
+        return `Your order ${id} will arrive in 20 minutes.`
+    }
+
+    cancelOrder(id) {
+        this.orders = this.orders.filter(order => order.id !== id)
+        return `You have canceled your order ${id}`
+    }
+}
