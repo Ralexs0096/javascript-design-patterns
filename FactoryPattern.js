60130c338ec6ae4bab60cff7fab588808ba8f12b
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-28 18:23:48 -0600

    Factory Pattern - 1

---
+++ FactoryPattern.js
@@ -0,0 +1,30 @@
+/**
+ * A function is a factory function when it returns
+ * a new object without the use of the `new` keyword.
+ */
+
+const createUser = ({firstName, lastName, email}) => ({
+    firstName,
+    lastName,
+    email,
+    fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+})
+
+// now we can create multiple users:
+
+const user1 = createUser({
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com"
+});
+
+const user2 = createUser({
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@doe.com"
+});
+
+console.log(user1);
+console.log(user2);
